324b6b3d1af8a1f0ebf289a7cb7c75503acc58b1
DaniloHY <nilo.11.dhy@gmail.com>
2022-09-28 20:09:34 -0300

    array

--- array/array.php
+++ array/array.php
@@ -51,7 +51,39 @@
 
 		<?php 
 
+			$texto = "micro computador";
 			
+			$tamanho = strlen($texto);
+
+			echo "</br>".$texto. " possui ". $tamanho. " caracteres";
+
+			echo "</br>".strtoupper($texto);
+
+			echo "</br>".strtolower($texto);
+
+			echo "</br>".ucfirst($texto);
+
+			echo "</br>".ucwords($texto);
+
+			echo "</br>".substr($texto,13,3);
+
+			echo "</br>".str_replace("micro", "Nano", $texto);
+
+		?>
+
+		<?php 
+
+		$dataAtual = date("d/m/y");
+
+		echo "</br> A data de hoje é: ".$dataAtual. "</br>";
+
+		$hora = date("h:i:s");
+
+		echo "</br> Agora são: ".$hora. "</br>";
+
+		$dataHora = date("d/m/y h:i:s");
+
+		echo "</br> Data e hora: ".$dataHora. "</br>";
 
 		?>
 
