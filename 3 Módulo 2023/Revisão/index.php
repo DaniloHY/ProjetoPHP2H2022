26e65b0488543a352a92af38b559d71d03ac5b07
DaniloHY <nilo.11.dhy@gmail.com>
2023-02-13 22:11:47 -0300

    CRUD

--- 3 Módulo 2023/Revisão/index.php
+++ 3 Módulo 2023/Revisão/index.php
@@ -1,12 +1,3 @@
-<?php 
-
-  include_once 'php_action/conexao_bd.php';
-
-  //cabeçalho
-  include_once 'includes/header.php';
-
-  include_once 'includes/mensagem.php';
-?>
 <!DOCTYPE html>
 <html>
 <head>
