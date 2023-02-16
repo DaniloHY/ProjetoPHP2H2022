6cadd6c500dcd3c2dcdbdf4882b0b8487c7149bd
DaniloHY <nilo.11.dhy@gmail.com>
2023-02-15 21:18:22 -0300

    Implementado

---
+++ 3 Módulo 2023/acao_php/conexao_bd.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$server  = 'localhost';
+	$user = 'admin';
+	$password = '123456';
+	$nome_db = 'dbFuncionarios';
+
+	$connection = mysqli_connect($server, $user, $password, $nome_db);
+
+	mysqli_set_charset($connection, "utf8");
+
+	if (mysqli_connect_error()) {
+		echo "Erro ao conectar com o banco de dados". mysql_connect_error();
+	}
+
+?>
