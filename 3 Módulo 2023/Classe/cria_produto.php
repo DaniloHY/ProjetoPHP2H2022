a5e2ee69c8721cb87c769a7b7137b50db77f85ab
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-06 21:50:42 -0300

    Classes

--- 3 Módulo 2023/Classe/cria_produto.php
+++ 3 Módulo 2023/Classe/cria_produto.php
@@ -4,6 +4,7 @@
 
 	$produto1 = new Produto;
 	$produto2 = new Produto;
+	$produto3 = new Produto;
 
 	$produto1->Codigo = 4001;
 	$produto1->Descricao = 'CD - The Best of Eric Clapton';
@@ -11,8 +12,14 @@
 	$produto2->Codigo = 4002;
 	$produto2->Descricao = "CD - The Eagles Hotel California";
 
+	$produto3->Codigo = 4003;
+	$produto3->Descricao = "Teclado";
+	$produto3->Preco = "100.00";
+	$produto3->Porcentagem = 10;
+
 	//Imprime informações de etiqueta
 	$produto1->ImprimeEtiqueta();
 	$produto2->ImprimeEtiqueta();
+	$produto3->Desconto();
 
 ?>
