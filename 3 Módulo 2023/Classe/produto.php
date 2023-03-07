df02dee8f992d31dd7a44d9960482e6cfd12fec1
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-06 22:00:11 -0300

    Classes

--- 3 Módulo 2023/Classe/produto.php
+++ 3 Módulo 2023/Classe/produto.php
@@ -17,8 +17,9 @@
 			print 'Codigo: '. $this->Codigo. "<br>";
 			print 'Descrição: '. $this->Descricao. "<br>";
 			print 'Porcentagem de Desconto: '. $this->Porcentagem. "%<br>";
-			print 'Valor do Produto R$ '. $this->Preco. "<br>";
-			print 'Valor total com desconto: R$ '. $this->Preco - ( $this->Preco * $this->Porcentagem / 100);
+			print 'Valor do Produto: R$ '. $this->Preco. "<br>";
+			print 'Valor total com desconto: R$ '. $this->Preco - ( $this->Preco * $this->Porcentagem / 100). "<br>";
+			print 'Quantidade: '. $this->Quantidade. "<br>";
 		}
 
 	}
