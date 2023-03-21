6603a88a74f0db23fe8f1b3a2d862ecc1e6ebd7c
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-20 21:32:08 -0300

    Classes

--- 3 Módulo 2023/Classe/pessoa.php
+++ 3 Módulo 2023/Classe/pessoa.php
@@ -30,7 +30,22 @@
 			}
 		}
 
+		//Método construtor inicializa propriedades
+		
+		function __construct($codigo, $nome, $altura, $idade, $nascimento, $escolaridade, $salario)
+		{
+			$this->codigo = $codigo;
+			$this->nome = $nome;
+			$this->altura = $altura;
+			$this->idade = $idade;
+			$this->nascimento = $nascimento;
+			$this->escolaridade = $escolaridade;
+			$this->salario = $salario;
+		}
 
+		//Método destrutor finaliza objeto
+
+		function
 
 
 
