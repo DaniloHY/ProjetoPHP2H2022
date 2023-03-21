779e2e148bd89130221333a7e6663918191dc8a1
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-20 22:12:01 -0300

    Classes

--- 3 Módulo 2023/Classe/ContaCorrente.php
+++ 3 Módulo 2023/Classe/ContaCorrente.php
@@ -10,6 +10,25 @@
 			parent:: __construct($agencia, $codigo, $dataDeCriacao, $titular, $senha, $saldo);
 			$this->limite = $limite;
 		}
+		//Método retirar (sobrescrito) verifica a $quantia
+		//Retirada esá dentro do limite
+		function Retirar($quantia){
+			//imposto sobre movimentação financeira
+			$cpmf = 0.05;
+
+			if (($this->saldo + $this->limite) >= $quantia)  {
+				//Executa método da classe-pai
+					parent::retirar($quantia);
+					//Debita o imposto
+					parent::retirar($quantia * $cpmf);
+			}
+			else {
+				echo "<br>Retirada não permitida ... <br>";
+				return false;
+			}
+			//retirada permitida
+			return true;
+		}
 	}
 
 ?>
