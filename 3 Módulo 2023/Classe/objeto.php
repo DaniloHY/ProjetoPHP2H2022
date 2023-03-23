5e5cd982d7e7244a64f52fec12602222029db58c
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-22 22:10:26 -0300

    Construtor

--- 3 Módulo 2023/Classe/objeto.php
+++ 3 Módulo 2023/Classe/objeto.php
@@ -25,4 +25,20 @@
 	$carlos->envelhecer(1);
 	echo "{$carlos->Nome} possui {$carlos->Idade} anos <br>";
 
+
+	$conta_carlos = new Conta;
+	$conta_carlos->Agencia = 6677;
+	$conta_carlos->Codigo = "CC.12 34. 56";
+	$conta_carlos->DataDeCriacao = "10/07/02";
+	$conta_carlos->Titular = $carlos;
+	$conta_carlos->Senha = 9876;
+	$conta_carlos->Saldo = 567.89
+	$conta_carlos->Cancelada = false;
+
+	echo "<br>";
+	echo "Manipulando a conta de: {$conta_carlos->Titular->Nome} <br>";
+	echo "O saldo atual é R/$ {$conta_carlos->obterSaldo()} <br>";
+
+	$conta_carlos->Depositar(20);
+	echo "O saldo atual é R/$ {$conta_carlos->obterSaldo()} <br>";
 ?>
