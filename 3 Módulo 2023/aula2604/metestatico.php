0fc91438e1d3b8f3e7df9f503367f2621e12f1f8
DaniloHY <nilo.11.dhy@gmail.com>
2023-05-03 21:26:18 -0300

    Aula0305

---
+++ 3 Módulo 2023/aula2604/metestatico.php
@@ -0,0 +1,20 @@
+<?php 
+
+	class Aplicacao{
+
+
+		static function Sobre(){
+
+			$fd = fopen('readme.txt', 'r');
+
+			while ($linha = fgets($fd, 200)) {
+				echo $linha;
+			}
+		}
+	}
+
+	echo "Informações da aplicação: <br>";
+	echo "==============================<br>";
+	Aplicacao::Sobre();
+
+?>
