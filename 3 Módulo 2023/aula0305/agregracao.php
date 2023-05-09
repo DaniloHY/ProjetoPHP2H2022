01bf73ad1920d9b8941504bcce3fe78a8375fa1d
DaniloHY <nilo.11.dhy@gmail.com>
2023-05-08 21:20:11 -0300

    Agregração

---
+++ 3 Módulo 2023/aula0305/agregracao.php
@@ -0,0 +1,11 @@
+<?php 
+
+	include_once 'cesta.php';
+	include_once 'produto.php';
+
+	$produto1 = new Produto;
+	$produto2 = new Produto;
+	$produto3 = new Produto;
+	$produto4 = new Produto;
+
+?>
---
+++ 3 Módulo 2023/aula0305/cesta.php
@@ -0,0 +1,32 @@
+<?php 
+
+	class Cesta{
+
+
+		private $itens;
+
+		#adiciona itens na cesta
+		function AdicionaItem(Produto $item){
+			$this->itens[] = $itens;
+		}
+
+		function ExibeLista (){
+			foreach ($this->$itens as $item) {
+				$item->ImprimeEtiqueta();
+			}
+		}
+
+		function CalculaTotal (){
+
+			$total = 0;
+			foreach ($this->itens as $item){
+
+				$total += $item->Preco;
+			}
+			return 'Total: R$ ' . $total . '<br>'; 
+		}
+
+
+	}
+
+?>
