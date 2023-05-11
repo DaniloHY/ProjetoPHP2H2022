906d5e803fb1812031a67662a798f0be3672af65
DaniloHY <nilo.11.dhy@gmail.com>
2023-05-10 22:01:46 -0300

    Exercício

--- 3 Módulo 2023/atividade1005/agregacao.php
+++ 3 Módulo 2023/atividade1005/agregacao.php
@@ -4,20 +4,15 @@
 	include_once 'produto.php';
 	include_once 'pedido.php';
 
+	$Pedido = new Pedido;
+	$Pedido->NroPedido = 2;
+	$Pedido->DatPedido = '10-05-2023';
+
 	$produto1 = new Produto;
 
 	$produto1->DesPro = "Um produto";
 	$produto1->QtdeProduto = 2;
 	$produto1->ValProduto = 1.40;
-
-
-	$itensPedidos = new ItensPedidos;
-
-	$itensPedidos->AdicionaItem($produto1);
-
-	echo $itensPedidos->CalculaTotal();
-	echo "<br>"; //quebra de linha
-	echo $itensPedidos->ExibeLista();
 
 
 ?>
--- 3 Módulo 2023/atividade1005/itensPedidos.php
+++ 3 Módulo 2023/atividade1005/itensPedidos.php
@@ -3,10 +3,25 @@
 	class ItensPedidos{
 		private $QtdeItem;
 		private $ValUnidade;
+		var $itens;
 
 		function Adicionaltem(Produto $item){
 			$this->itens[] = $item;
 		}
+		function ExibeLista (){
+			foreach ($this->itens as $item) {
+				$item->ImprimeEtiqueta();
+			}
+		}
+		function ValorUnidade(){
+			$ValUnidade = 0;
+			foreach ($this->itens as $item){
+
+				$ValUnidade = $item->QtdeProduto * $item->ValProduto;
+			}
+			return 'Total: R$ ' . $ValUnidade . '<br>'; 
+		}
+
 	}
 
 ?>
